34dd7cf5525e4920752cf38565afc8c5eea74a08
NDA <anhndgcc19072@fpt.edu.vn>
2024-09-20 19:58:46 +0700

    nerf Honeyed Chicken (60s -> 10s Slow Falling).
    nerf Sweet Melon Smoothie (10 hunger points -> 6 hunger points)
    
    added food properties and textures for honeyed potato, kelp-wrapped and dried kelp-wrapped foods.
    
    added texture for Paper Stack.
    
    updated mod version (1.1)

--- src/main/java/net/natevg/fnf/item/FnFFoodProperties.java
+++ src/main/java/net/natevg/fnf/item/FnFFoodProperties.java
@@ -38,10 +38,16 @@ public class FnFFoodProperties {
             .effect(new MobEffectInstance(MobEffects.MOVEMENT_SPEED, 1200, 1), 1.0f)
             .effect(new MobEffectInstance(MobEffects.HEAL, 0), 1.0f)
             .build();
+    public static final FoodProperties HONEYED_POTATO = new FoodProperties.Builder()
+            .nutrition(5)
+            .saturationModifier(0.6f)
+            .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 1200), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.HEAL, 0), 1.0f)
+            .build();
     public static final FoodProperties HONEYED_CHICKEN = new FoodProperties.Builder()
             .nutrition(6)
             .saturationModifier(0.6f)
-            .effect(new MobEffectInstance(MobEffects.SLOW_FALLING, 1200), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.SLOW_FALLING, 200), 1.0f)
             .effect(new MobEffectInstance(MobEffects.HEAL, 0), 1.0f)
             .build();
 
@@ -63,8 +69,53 @@ public class FnFFoodProperties {
             .effect(new MobEffectInstance(MobEffects.DAMAGE_BOOST, 1200), 1.0f)
             .effect(new MobEffectInstance(MobEffects.HEAL, 0), 1.0f)
             .build();
+    public static final FoodProperties KELP_WRAPPED_COD = new FoodProperties.Builder()
+            .nutrition(2)
+            .saturationModifier(0.2f)
+            .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 200), 1.0f)
+            .build();
+    public static final FoodProperties DRIED_KELP_WRAPPED_COD = new FoodProperties.Builder()
+            .nutrition(8)
+            .saturationModifier(0.8f)
+            .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 2400), 1.0f)
+            .build();
+    public static final FoodProperties KELP_WRAPPED_SALMON = new FoodProperties.Builder()
+            .nutrition(2)
+            .saturationModifier(0.2f)
+            .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 400), 1.0f)
+            .build();
+    public static final FoodProperties DRIED_KELP_WRAPPED_SALMON = new FoodProperties.Builder()
+            .nutrition(8)
+            .saturationModifier(0.8f)
+            .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 3600), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.DOLPHINS_GRACE, 2400), 1.0f)
+            .build();
+    public static final FoodProperties KELP_WRAPPED_TROPICAL_FISH = new FoodProperties.Builder()
+            .nutrition(2)
+            .saturationModifier(0.2f)
+            .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 600), 1.0f)
+            .build();
+    public static final FoodProperties DRIED_KELP_WRAPPED_TROPICAL_FISH = new FoodProperties.Builder()
+            .nutrition(8)
+            .saturationModifier(0.8f)
+            .effect(new MobEffectInstance(MobEffects.CONDUIT_POWER, 4800), 1.0f)
+            .build();
+    public static final FoodProperties KELP_WRAPPED_VEGGIE_STICKS = new FoodProperties.Builder()
+            .nutrition(4)
+            .saturationModifier(0.3f)
+            .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 200), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 100), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 100), 1.0f)
+            .build();
+    public static final FoodProperties DRIED_KELP_WRAPPED_VEGGIE_STICKS = new FoodProperties.Builder()
+            .nutrition(6)
+            .saturationModifier(0.7f)
+            .effect(new MobEffectInstance(MobEffects.WATER_BREATHING, 1200), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.DIG_SPEED, 600), 1.0f)
+            .effect(new MobEffectInstance(MobEffects.NIGHT_VISION, 600), 1.0f)
+            .build();
     public static final FoodProperties SWEET_MELON_SMOOTHIE = new FoodProperties.Builder()
-            .nutrition(10)
+            .nutrition(6)
             .alwaysEdible()
             .effect(new MobEffectInstance(MobEffects.REGENERATION, 400, 1), 1.0f)
             .effect(new MobEffectInstance(MobEffects.FIRE_RESISTANCE, 1200, 1), 1.0f)
